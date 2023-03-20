cdc4c25997e65f16b36d7e1b0ca026780897e479
Ali <alibaniasad1999@yahoo.com>
2023-03-21 02:33:01 +0330

    make code better but question is wrong

--- HW/HW1/Code/Q3/Q3.m
+++ HW/HW1/Code/Q3/Q3.m
@@ -1,6 +1,6 @@
 clc
 clear
-%% Question 3 %%
+%% Part I %%
 
 % data
 gps_1 = [8.27, -6.23, 16.74]*1e6; % km
@@ -34,4 +34,19 @@ d = sym('d', 'real');
 
 eq = (x_gps - x).^2 + (y_gps - y).^2 + (z_gps - z).^2 - (c*(t_gps - d)).^2 ...
     == 0;
+
+%% part II %% calculate all permutation
+
+eq = subs(eq, d, 0.05);
+all_eq = nchoosek(eq, 3);
+
+for i = 1:length(all_eq)
+    ans_gps = vpasolve(all_eq(i));
+    fprintf("%d conition x=%.2f, y=%.2f, z=%.2f\n", i, ans_gps.x, ans_gps.y,...
+        ans_gps.z)
+    if isempty(ans_gps.x)
+        fprintf('no valid answer\n');
+    end
+end
+
 
