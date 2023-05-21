7e9ad6d2be7cd1389ed4a76f195a1a5dc6cedeff
Ali <alibaniasad1999@yahoo.com>
2023-05-21 06:25:05 +0330

    add part f

--- HW/HW3/Code/Q1/f/f_LA_vary.m
+++ HW/HW3/Code/Q1/f/f_LA_vary.m
@@ -1,10 +1,19 @@
+clear;
+clc;
 %% calculation of control effort in vary of LA parameter %%
-
-for LA = 20:5:80
+LA = 20:5:80;
+MD = zeros(1, length(LA)); % miss distance
+for i = 1:length(LA)
     modelname = "f";
     simIn = Simulink.SimulationInput(modelname);
     
-    simIn = setVariable(simIn,'LA',LA*pi/180);
+    simIn = setVariable(simIn,'LA',LA(i)*pi/180);
     
     simulation_data = sim(simIn);
+
+    MD(i) = simulation_data.miss_distance.Data(end);
+
+    fprintf('Look Angel:%.2f, i=%d\n', LA(i), i);
 end
+
+save('MD_LA_vary', "MD")
---
+++ HW/HW3/Code/Q1/f/ploter.m
@@ -0,0 +1,14 @@
+load('MD_LA_vary.mat')
+set(gca, 'FontSize', 16)
+plot(LA, MD, 'b', 'LineWidth', 2);
+xlabel('Look Angle$(\deg)$', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('Miss Distance(m)', 'interpreter', 'latex', 'FontSize', 24);
+axis tight
+set(gca, 'FontSize', 16, 'FontName', 'Times New Roman');
+
+[dir_state, ~, ~] = mkdir('../../../Figure/Q1/f');
+if dir_state
+    print('../../../Figure/Q1/f/MD','-depsc');
+else
+    fprintf("Ooooooops\n")
+end
