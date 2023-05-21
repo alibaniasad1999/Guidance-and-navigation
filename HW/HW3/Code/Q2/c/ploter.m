143249795fe819e16358b35478964cd32453e78c
Ali <alibaniasad1999@yahoo.com>
2023-05-21 18:49:12 +0330

    fix and add adjoint

--- HW/HW3/Code/Q2/c/ploter.m
+++ HW/HW3/Code/Q2/c/ploter.m
@@ -1,6 +1,6 @@
 clear;
 clc;
-%% APN %%
+%% APN %% 
 load('n_t_HE_T_vary.mat')
 set(gca, 'FontSize', 16)
 tout = 0:0.002:15;
@@ -8,12 +8,13 @@ plot(tout, HE_TPN(:, 1)*180/pi, 'LineWidth', 2);
 hold on;
 plot(tout, HE_APN(:, 1)*180/pi, 'LineWidth', 2);
 
-xlabel('$T_f(\sec)$', 'interpreter', 'latex', 'FontSize', 24);
-ylabel('Heading Error(m)', 'interpreter', 'latex', 'FontSize', 24);
+xlabel('$T_f(\sec)$', 'interpreter', 'latex', 'FontSize', 36);
+ylabel('$\frac{MD}{-V_m.HE.T}$', 'interpreter', 'latex', 'FontSize', 36);
 axis tight
 set(gca, 'FontSize', 16, 'FontName', 'Times New Roman');
 legend('TPN', 'APN', 'Location','northeast', 'FontSize', 20);
-
+xlabel('$T_f(\sec)$', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('$\frac{MD}{-V_m.HE.T}$', 'interpreter', 'latex', 'FontSize', 24);
 [dir_state, ~, ~] = mkdir('../../../Figure/Q2/c');
 if dir_state
     print('../../../Figure/Q2/c/HE_25','-depsc');
@@ -29,10 +30,12 @@ plot(tout, HE_TPN(:, 2)*180/pi, 'LineWidth', 2);
 hold on;
 plot(tout, HE_APN(:, 2)*180/pi, 'LineWidth', 2);
 
-xlabel('$T_f(\sec)$', 'interpreter', 'latex', 'FontSize', 24);
-ylabel('Heading Error(m)', 'interpreter', 'latex', 'FontSize', 24);
+xlabel('$T_f(\sec)$', 'interpreter', 'latex', 'FontSize', 36);
+ylabel('$\frac{MD}{-V_m.HE.T}$', 'interpreter', 'latex', 'FontSize', 36);
 axis tight
 set(gca, 'FontSize', 16, 'FontName', 'Times New Roman');
+xlabel('$T_f(\sec)$', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('$\frac{MD}{-V_m.HE.T}$', 'interpreter', 'latex', 'FontSize', 24);
 legend('TPN', 'APN', 'Location','northeast', 'FontSize', 20);
 
 [dir_state, ~, ~] = mkdir('../../../Figure/Q2/c');
@@ -48,10 +51,12 @@ plot(tout, HE_TPN(:, 3)*180/pi, 'LineWidth', 2);
 hold on;
 plot(tout, HE_APN(:, 3)*180/pi, 'LineWidth', 2);
 
-xlabel('$T_f(\sec)$', 'interpreter', 'latex', 'FontSize', 24);
-ylabel('Heading Error(m)', 'interpreter', 'latex', 'FontSize', 24);
+xlabel('$T_f(\sec)$', 'interpreter', 'latex', 'FontSize', 36);
+ylabel('$\frac{MD}{-V_m.HE.T}$', 'interpreter', 'latex', 'FontSize', 36);
 axis tight
 set(gca, 'FontSize', 16, 'FontName', 'Times New Roman');
+xlabel('$T_f(\sec)$', 'interpreter', 'latex', 'FontSize', 24);
+ylabel('$\frac{MD}{-V_m.HE.T}$', 'interpreter', 'latex', 'FontSize', 24);
 legend('TPN', 'APN', 'Location','northeast', 'FontSize', 20);
 
 [dir_state, ~, ~] = mkdir('../../../Figure/Q2/c');
@@ -60,3 +65,70 @@ if dir_state
 else
     fprintf("Ooooooops\n")
 end
+hold off;
+
+set(gca, 'FontSize', 16)
+tout = 0:0.002:15;
+plot(tout, n_t_TPN(:, 1)*180/pi, 'LineWidth', 2);
+hold on;
+plot(tout, n_t_APN(:, 1)*180/pi, 'LineWidth', 2);
+
+xlabel('$T_f(\sec)$', 'interpreter', 'latex', 'FontSize', 36);
+ylabel('$\frac{MD}{n_t.T^2}$', 'interpreter', 'latex', 'FontSize', 36);
+axis tight
+set(gca, 'FontSize', 16, 'FontName', 'Times New Roman');
+xlabel('$T_f(\sec)$', 'interpreter', 'latex', 'FontSize', 36);
+ylabel('$\frac{MD}{n_t.T^2}$', 'interpreter', 'latex', 'FontSize', 36);
+legend('TPN', 'APN', 'Location','northeast', 'FontSize', 20);
+
+[dir_state, ~, ~] = mkdir('../../../Figure/Q2/c');
+if dir_state
+    print('../../../Figure/Q2/c/n_t_25','-depsc');
+else
+    fprintf("Ooooooops\n")
+end
+hold off;
+
+set(gca, 'FontSize', 16)
+tout = 0:0.002:15;
+plot(tout, n_t_TPN(:, 2)*180/pi, 'LineWidth', 2);
+hold on;
+plot(tout, n_t_APN(:, 2)*180/pi, 'LineWidth', 2);
+
+xlabel('$T_f(\sec)$', 'interpreter', 'latex', 'FontSize', 36);
+ylabel('$\frac{MD}{n_t.T^2}$', 'interpreter', 'latex', 'FontSize', 36);
+axis tight
+set(gca, 'FontSize', 16, 'FontName', 'Times New Roman');
+xlabel('$T_f(\sec)$', 'interpreter', 'latex', 'FontSize', 36);
+ylabel('$\frac{MD}{n_t.T^2}$', 'interpreter', 'latex', 'FontSize', 36);
+legend('TPN', 'APN', 'Location','northeast', 'FontSize', 20);
+
+[dir_state, ~, ~] = mkdir('../../../Figure/Q2/c');
+if dir_state
+    print('../../../Figure/Q2/c/n_t_5','-depsc');
+else
+    fprintf("Ooooooops\n")
+end
+hold off;
+
+set(gca, 'FontSize', 16)
+tout = 0:0.002:15;
+plot(tout, n_t_TPN(:, 3)*180/pi, 'LineWidth', 2);
+hold on;
+plot(tout, n_t_APN(:, 3)*180/pi, 'LineWidth', 2);
+
+xlabel('$T_f(\sec)$', 'interpreter', 'latex', 'FontSize', 36);
+ylabel('$\frac{MD}{n_t.T^2}$', 'interpreter', 'latex', 'FontSize', 36);
+axis tight
+set(gca, 'FontSize', 16, 'FontName', 'Times New Roman');
+xlabel('$T_f(\sec)$', 'interpreter', 'latex', 'FontSize', 36);
+ylabel('$\frac{MD}{n_t.T^2}$', 'interpreter', 'latex', 'FontSize', 36);
+legend('TPN', 'APN', 'Location','northeast', 'FontSize', 20);
+
+[dir_state, ~, ~] = mkdir('../../../Figure/Q2/c');
+if dir_state
+    print('../../../Figure/Q2/c/n_t_1','-depsc');
+else
+    fprintf("Ooooooops\n")
+end
+hold off;
